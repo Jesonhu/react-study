967f2ce764a2309c59e00b4eb57f6bb7ecd48b85
Jesonhu <jesonhu_web@163.com>
2019-04-17 07:37:59 +0800

    feat: 组件间属性值传递.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import './App.css';
 // eslint-disable-next-line
 import HelloWord from './views/0101-helloWord'
 import HelloWordFn from './views/0102-function-components'
+import { HelloWordProps, HelloWordPropsFn } from './views/0103-props/index';
 
 class App extends Component {
   render() {
+    const expressProps = '我是表达式属性';
     return (
-      <HelloWordFn />
+      <div>
+        <HelloWordProps 
+          hello="类组件方式"
+          expressProps={expressProps}
+        />
+        <HelloWordPropsFn 
+          hello="函数式组件方式"
+          expressProps={expressProps}
+        />
+      </div>
     )
   }
 }
--- src/views/0103-props/index.js
+++ src/views/0103-props/index.js
@@ -1,5 +1,29 @@
 import React, { Component } from 'react';
 
-export default class HelloWord extends React.Component {
-  
+/**
+ * 类组件方式传参.
+ *
+ * @export
+ * @class HelloWordProps
+ * @extends {React.Component}
+ */
+export class HelloWordProps extends React.Component {
+  render() {
+    return (
+      <h1>Hello Word {this.props.hello}-{this.props.expressProps}</h1>
+    )
+  }
+}
+
+/**
+ * 函数是组件方式传参.
+ *
+ * @export
+ * @param {*} props
+ * @returns
+ */
+export function HelloWordPropsFn(props) {
+  return (
+    <h1>Hello Word {props.hello}-{props.expressProps}</h1>
+  )
 }
